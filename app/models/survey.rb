eaef25e0a9d43cdd4ab29bbc37fdc7c740a5373d
An Tran <tranminhan.my@gmail.com>
2016-08-11 11:45:15 +0700

    generate sample responses

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -22,4 +22,33 @@ class Survey < ApplicationRecord
       Collaboration::SURVEY_ACTIONS[role.to_sym].include?(action)
     end
   end
+
+  def generate_response
+    puts "generate response for #{self.inspect}"
+    transaction do
+      r = responses.create!
+      questions.each do |q|
+        puts "--> generate answer for #{q.inspect}"
+        case q.type.to_sym
+          when :short_answer
+            content = %w(harley thuan an anta thinh thanh).sample
+            q.choices.each do |c|
+              r.answers.create! question: q, choice: c, content: content
+            end
+          when :multiple_choice
+            c = q.choices.sample
+            r.answers.create! question: q, choice: c, content: c.content
+          # ...
+          when :check_boxes
+            size = q.choices.size # .length, .count
+            num  = rand(size) + 1
+            q.choices.shuffle.take(num).each do |c|
+              r.answers.create! question: q, choice: c, content: c.content
+            end
+          else
+            raise "Unknown type: #{q.type}"
+        end
+      end
+    end
+  end
 end
